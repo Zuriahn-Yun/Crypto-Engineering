b593d4426e14ba3002ac98e77df02471459df7c6
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-06-29 21:29:57 -0700

    requirmentsss

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@
 import uvicorn
 from extract import bitcoin_main, coin_data
 from fastapi import FastAPI, Query
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 app = FastAPI()
 
@@ -17,9 +19,28 @@ def get_bitcoin_data():
     return bitcoin_main()
 
 @app.get("/coin_data")
-def get_coin_data(coin_id: str = Query(..., description="Coin ID from CoinGecko (e.g., 'ethereum', 'cardano')")):
+def get_coin_plot(coin_id: str = Query(..., description="Coin ID from CoinGecko (e.g., 'ethereum', 'cardano')")):
     """Get candlestick and Heikin Ashi data for any coin, query prompts for coin ID"""
-    return coin_data(coin_id)
+    coin_df,heiken_df = coin_data(coin_id)
+    fig = make_subplots(rows=1, cols=1, subplot_titles=("Candles", "Heiken Ashi Candles"))
+
+    fig.add_trace(go.Candlestick(x=coin_df['timestamp'],
+                    open=coin_df['open'],
+                    high=coin_df['high'],
+                    low=coin_df['low'],
+                    close=coin_df['close'],name="Traditional Candles"))
+
+
+    fig.add_trace(go.Candlestick(x=heiken_df['timestamp'],
+                    open=heiken_df['ha_open'],
+                    high=heiken_df['ha_high'],
+                    low=heiken_df['ha_low'],
+                    close=heiken_df['ha_close'],name="Heiken Ashi Candles"))
+
+    fig.update_layout(title=dict(text="Bitcoin Stock Data From the last Day"))
+
+    return fig.to_html(full_html = False)
+
 
 @app.get("/")
 def root():
