bc11bfd60f8acf026335e99865332c823de9afa6
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-02 11:05:10 -0700

    api prison

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 # main.py - Fixed FastAPI server
 import uvicorn
-from extract import bitcoin_main, coin_data
+from extract import bitcoin_main, coin_data,convert_miliseconds_datetime
 from fastapi import FastAPI, Query
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
@@ -24,6 +24,11 @@ def get_coin_plot(coin_id: str = Query(..., description="Coin ID from CoinGecko 
     try:
         """Get candlestick and Heikin Ashi data for any coin, query prompts for coin ID"""
         coin_df,heiken_df = coin_data(coin_id)
+        
+        coin_df['timestamp'] = coin_df['timestamp'].apply(convert_miliseconds_datetime)
+        
+        heiken_df['timestamp'] = heiken_df['timestamp'].apply(convert_miliseconds_datetime)
+        
         fig = make_subplots(rows=1, cols=1, subplot_titles=("Candles", "Heiken Ashi Candles"))
 
         fig.add_trace(go.Candlestick(x=coin_df['timestamp'],
@@ -43,11 +48,11 @@ def get_coin_plot(coin_id: str = Query(..., description="Coin ID from CoinGecko 
         html = fig.to_html(include_plotlyjs='cdn',full_html = True)
         
         return HTMLResponse(html)
+    
     except Exception as e:
         import traceback
         traceback.print_exc()
         return HTMLResponse(content=f"<h3>Internal Error:</h3><pre>{str(e)}</pre>", status_code=500)
-
 
 @app.get("/")
 def root():
